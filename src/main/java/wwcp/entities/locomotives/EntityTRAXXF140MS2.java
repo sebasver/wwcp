83663b466cec8a3e65cf5067718970ba6ade4ac9
sebasver68@gmail.com <Sv13082001>
2019-10-21 20:22:30 +0200

    New skin addition, Further work expected later

--- src/main/java/wwcp/entities/locomotives/EntityTRAXXF140MS2.java
+++ src/main/java/wwcp/entities/locomotives/EntityTRAXXF140MS2.java
@@ -61,7 +61,7 @@ import net.minecraft.init.Items;
         @Override
         public void registerSkins(){
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T1.png", "textures/bogies/Flexxpower.png","Belgian Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
-            SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T2.png", "textures/bogies/Flexxpower.png","NS Livery", "Used by the NMBS/SNCB in belgium for freight trains");
+            SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T2.png", "textures/bogies/Flexxpower.png","NS Livery", "Used by the NS in their regular service");
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T3.png", "textures/bogies/Flexxpower.png", "Polish Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T4.png", "textures/bogies/Flexxpower.png","Easter Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T5.png", "textures/bogies/Flexxpower.png","Silesian Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
@@ -69,6 +69,7 @@ import net.minecraft.init.Items;
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T7.png", "textures/bogies/Flexxpower.png","Valentines Day Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T8.png", "textures/bogies/Flexxpower.png","WWCP Commercial Livery", "Used by the NMBS/SNCB in belgium for freight trains");
             SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T9.png", "textures/bogies/Flexxpower.png","StPatricks Day Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
+            SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Electric/F140MS2/T10.png", "textures/bogies/Flexxpower.png","Halloween Theme Skin", "Used by the NMBS/SNCB in belgium for freight trains");
        }
 
         @Override
--- src/main/java/wwcp/models/locomotives/F140MS2.java
+++ src/main/java/wwcp/models/locomotives/F140MS2.java
@@ -24,7 +24,7 @@ public class F140MS2 extends ModelBase //Same as Filename
 		bodyModel = new ModelRendererTurbo[395];
 
 		initbodyModel_1();
-		this.fixRotation(this.bodyModel, false, true, true);
+		flipAll();
 
 	}
 	private void initbodyModel_1()
