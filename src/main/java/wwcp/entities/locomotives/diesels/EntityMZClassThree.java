9565d060428c06d229272cc91cefa6ca5a3ec875
Henrik <henikojgaard@gmail.com>
2020-06-08 23:15:42 +0200

    Added textures to the newest accepted.
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/locomotives/diesels/EntityMZClassThree.java
+++ src/main/java/wwcp/entities/locomotives/diesels/EntityMZClassThree.java
@@ -90,6 +90,8 @@ public class EntityMZClassThree extends EntityTrainCore {
     public void registerSkins(){
         SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Diesel/MZ/MzIIIDSB1990-2000s.png", "textures/bogies/Mz_Bogey.png",
                 "DSB Red and Black Livery", "Standard Red and Black livery for the DSB");
+        SkinRegistry.addSkin(this.getClass(),worldwidecontentpack.MODID, "textures/locomotive/Diesel/MZ/MzIIIDSB1972-1990s.png", "textures/bogies/Mz_Bogey.png",
+                "DSB Red and Black Livery 1972", "Standard Red and Black livery for the DSB 1972 although plows were added the windows werent blinded until 1990);
     }
 
     @Override
