a4b7ed800940d8dbf59d1855bdc631844ce213c2
Henrik <henikojgaard@gmail.com>
2020-06-14 15:17:38 +0200

    Fixed Bogie position
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/freight/EntityZACNS.java
+++ src/main/java/wwcp/entities/freight/EntityZACNS.java
@@ -42,7 +42,7 @@ public class EntityZACNS extends GenericRailTransport {
     }
 
     public float[] bogieLengthFromCenter() {
-        return new float[]{1.0F, -1.0F};
+        return new float[]{0.5F, -0.5F};
     }
 
     public float getRenderScale() {
@@ -146,7 +146,7 @@ public class EntityZACNS extends GenericRailTransport {
     }
 
     public float[] getHitboxSize() {
-        return new float[]{8.f, 2f, 1.6F};
+        return new float[]{8.5f, 2f, 1.6F};
     }
 
     public float getPistonOffset() {
