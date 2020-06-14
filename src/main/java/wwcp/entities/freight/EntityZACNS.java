54d2307ea4626da9dd1d2ba43438251d7a16f795
Henrik <henikojgaard@gmail.com>
2020-06-14 15:50:10 +0200

    Fixed Bogie pos
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/freight/EntityZACNS.java
+++ src/main/java/wwcp/entities/freight/EntityZACNS.java
@@ -33,7 +33,7 @@ public class EntityZACNS extends GenericRailTransport {
     }
     @Override
     public float[][] bogieModelOffsets() {
-        return new float[][]{{2.5f,0.08f,0},{-2.5f,0.08f,0}};
+        return new float[][]{{2.55f,0.08f,0},{-2.5f,0.08f,0}};
     }
 
     @Override
