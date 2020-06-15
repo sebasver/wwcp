6947b490cb96149e6270be1b352d48b44111b7e4
Henrik <henikojgaard@gmail.com>
2020-06-16 01:23:04 +0200

    Fixing Bogie Offset
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/freight/EntityEALOSX.java
+++ src/main/java/wwcp/entities/freight/EntityEALOSX.java
@@ -33,7 +33,7 @@ public class EntityEALOSX extends GenericRailTransport {
     }
     @Override
     public float[][] bogieModelOffsets() {
-        return new float[][]{{2.12F,0.08f,0},{-2.12F,0.08f,0}};
+        return new float[][]{{2.1225F,0.08f,0},{-2.1225F,0.08f,0}};
     }
 
     @Override
@@ -42,7 +42,7 @@ public class EntityEALOSX extends GenericRailTransport {
     }
 
     public float[] bogieLengthFromCenter() {
-        return new float[]{2.12F, -2.12F};
+        return new float[]{2.1225F, -2.1225F};
     }
 
     public float getRenderScale() {
