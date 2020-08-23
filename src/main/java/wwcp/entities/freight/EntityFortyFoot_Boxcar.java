0e290e415973eef5aac5570fde1685d86979cf51
Henrik <henikojgaard@gmail.com>
2020-08-23 23:33:12 +0200

    Bogie Offset changed
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/freight/EntityFortyFoot_Boxcar.java
+++ src/main/java/wwcp/entities/freight/EntityFortyFoot_Boxcar.java
@@ -33,7 +33,7 @@ public class EntityFortyFoot_Boxcar extends GenericRailTransport {
 
     @Override
     public float[][] bogieModelOffsets() {
-        return new float[][]{{2f,0.08f,0},{-2f,0.08f,0}};
+        return new float[][]{{2.05f,0.08f,0},{-2.05f,0.08f,0}};
     }
 
     @Override
