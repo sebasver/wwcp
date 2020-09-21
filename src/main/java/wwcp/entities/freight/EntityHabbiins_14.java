fba61bd5a254191ad5dd9b08f1cca5684d7d5e51
Henrik <henikojgaard@gmail.com>
2020-09-21 22:35:10 +0200

    Hitbox changes
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/freight/EntityHabbiins_14.java
+++ src/main/java/wwcp/entities/freight/EntityHabbiins_14.java
@@ -112,7 +112,7 @@ public class EntityHabbiins_14 extends GenericRailTransport {
         }
 
         public float[] getHitboxSize() {
-            return new float[]{10.5f, 2f, 1.6F};
+            return new float[]{11f, 2f, 1.6F};
         }
 
         public float getPistonOffset() {
