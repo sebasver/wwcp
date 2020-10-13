ece0dc0344d2df01f0a96cf3ed66ef22c9d5be20
Henrik <henikojgaard@gmail.com>
2020-10-14 01:17:20 +0200

    Added the Flatbed as Entity
    Signed-off-by: Henrik <henikojgaard@gmail.com>

---
+++ src/main/java/wwcp/entities/freight/EntityUKFlatbed.java
@@ -0,0 +1,127 @@
+package wwcp.entities.freight;
+
+import ebf.tim.TrainsInMotion;
+import ebf.tim.api.SkinRegistry;
+import ebf.tim.entities.GenericRailTransport;
+import ebf.tim.utility.RailUtility;
+import fexcraft.tmt.slim.ModelBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import wwcp.entities.EntityDataSets.Transport;
+import wwcp.entities.WWCPTransport;
+import wwcp.models.freight.UKFlatbed;
+import wwcp.worldwidecontentpack;
+
+import java.util.UUID;
+
+public class EntityUKFlatbed extends GenericRailTransport {
+
+    public static final String[] itemDescription = new String[]{"§7" + StatCollector.translateToLocal("menu.item.weight") + ": 2" + StatCollector.translateToLocal("menu.item.tons"), "§7" + StatCollector.translateToLocal("menu.item.sizeof") + ": 27" + StatCollector.translateToLocal("menu.item.slots")};
+
+    public static final Item thisItem = new WWCPTransport(new EntityUKFlatbed((World)null), worldwidecontentpack.MODID, worldwidecontentpack.United_Kingdom);
+
+    public EntityUKFlatbed(UUID owner, World world, double xPos, double yPos, double zPos) {
+        super(owner, world, xPos, yPos, zPos);
+    }
+
+    public EntityUKFlatbed(World world) {
+        super(world);
+    }
+
+    public float[][] bogieModelOffsets() {
+        return null;}
+
+    public ModelBase[] bogieModels() {
+        return null;
+    }
+
+    public float[] bogieLengthFromCenter() {
+        return new float[]{1.0F, -1.0F};
+    }
+
+    public float getRenderScale() {
+        return 0.0625F;
+    }
+
+    @Override
+    public float[][] modelOffsets() {
+        return new float[][]{{0.625f,-0.10F,0.065F}};
+    }
+
+    public void registerSkins() {
+        SkinRegistry.addSkin(this.getClass(), worldwidecontentpack.MODID, "textures/freightskins/UKopenwagon/UK_Flatbed_Texture_1.png",
+                "Default Flat wagon",
+                "This was the factory skin of the car");
+    }
+
+    @Override
+    public String transportFuelType() {
+        return null;
+    }
+
+    public boolean isReinforced() {
+        return Transport.UKFlatbed().reinforced;
+    }
+
+    public float weightKg() {
+        return Transport.UKFlatbed().weightinKGs;
+    }
+
+    public ItemStack[] getRecipie() {
+        return new ItemStack[]{RailUtility.DefineStack(Blocks.iron_ore, 1), null, null, null, null, null, null, null, null};
+    }
+
+    public String transportName() {
+        return Transport.UKFlatbed().name;
+    }
+
+    public String transportcountry() { return Transport.UKFlatbed().country; }
+
+    public String transportYear() { return Transport.UKFlatbed().year; }
+
+    public float transportTopSpeed() {
+        return Transport.UKFlatbed().topSpeed;
+    }
+
+    public boolean isFictional() {
+        return Transport.UKFlatbed().fictional;
+    }
+
+    @Override
+    public String[] additionalItemText() {
+        {return new String[]{RailUtility.translate(Transport.UKFlatbed().additionalTextTitle) + Transport.UKFlatbed().additionalText,
+                RailUtility.translate(Transport.UKFlatbed().additionalTextTitle2) + Transport.UKFlatbed().additionalText2};}
+    }
+    public int getInventoryRows() {
+        return Transport.UKFlatbed().rows;
+    }
+
+    public TrainsInMotion.transportTypes getType() {
+        return TrainsInMotion.transportTypes.FREIGHT;
+    }
+
+    public float[][] getRiderOffsets() {
+        return (float[][])null;
+    }
+
+    public float[] getHitboxSize() {
+        return new float[]{2.95f, 1.4f, 1.4F};
+    }
+
+    public float getPistonOffset() {
+        return 0.0F;
+    }
+
+    public ModelBase[] getModel() {
+        return new ModelBase[]{new UKFlatbed()};
+    }
+
+    public Item getItem() {
+        return thisItem;
+    }
+
+
+}
