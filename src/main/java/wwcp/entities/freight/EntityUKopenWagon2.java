38dc8159b0977c655849890f0780f89ac89b9b4f
Henrik <henikojgaard@gmail.com>
2020-10-14 16:22:10 +0200

    Added Windcutter Entity
    Signed-off-by: Henrik <henikojgaard@gmail.com>

---
+++ src/main/java/wwcp/entities/freight/EntityUKopenWagon2.java
@@ -0,0 +1,130 @@
+package wwcp.entities.freight;
+
+import ebf.tim.TrainsInMotion;
+import ebf.tim.api.SkinRegistry;
+import ebf.tim.entities.GenericRailTransport;
+import ebf.tim.utility.RailUtility;
+import fexcraft.tmt.slim.ModelBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import wwcp.entities.EntityDataSets.Transport;
+import wwcp.entities.WWCPTransport;
+import wwcp.models.freight.UKopenwagon2;
+import wwcp.worldwidecontentpack;
+
+import java.util.UUID;
+
+public class EntityUKopenWagon2 extends GenericRailTransport {
+
+    public static final String[] itemDescription = new String[]{"§7" + StatCollector.translateToLocal("menu.item.weight") + ": 2" + StatCollector.translateToLocal("menu.item.tons"), "§7" + StatCollector.translateToLocal("menu.item.sizeof") + ": 27" + StatCollector.translateToLocal("menu.item.slots")};
+
+    public static final Item thisItem = new WWCPTransport(new EntityUKopenWagon2((World)null), worldwidecontentpack.MODID, worldwidecontentpack.United_Kingdom);
+
+    public EntityUKopenWagon2(UUID owner, World world, double xPos, double yPos, double zPos) {
+        super(owner, world, xPos, yPos, zPos);
+    }
+
+    public EntityUKopenWagon2(World world) {
+        super(world);
+    }
+
+    public float[][] bogieModelOffsets() {
+        return null;}
+
+    public ModelBase[] bogieModels() {
+        return null;
+    }
+
+    public float[] bogieLengthFromCenter() {
+        return new float[]{1.0F, -1.0F};
+    }
+
+    public float getRenderScale() {
+        return 0.0625F;
+    }
+
+    @Override
+    public float[][] modelOffsets() {
+        return new float[][]{{0.625f,-0.10F,0.065F}};
+    }
+
+    public void registerSkins() {
+        SkinRegistry.addSkin(this.getClass(), worldwidecontentpack.MODID, "textures/freightskins/UKopenwagon/WC1.png",
+                "Grey Windcutter",
+                "This was the factory skin of the car");
+        SkinRegistry.addSkin(this.getClass(), worldwidecontentpack.MODID, "textures/freightskins/UKopenwagon/WC2.png",
+                "Brown Windcutter",
+                "This is a Brown Windcutter");
+    }
+
+    @Override
+    public String transportFuelType() {
+        return null;
+    }
+
+    public boolean isReinforced() {
+        return Transport.UKOpenWagon().reinforced;
+    }
+
+    public float weightKg() {
+        return Transport.UKOpenWagon().weightinKGs;
+    }
+
+    public ItemStack[] getRecipie() {
+        return new ItemStack[]{RailUtility.DefineStack(Blocks.iron_ore, 1), null, null, null, null, null, null, null, null};
+    }
+
+    public String transportName() {
+        return Transport.UKOpenWagon().name;
+    }
+
+    public String transportcountry() { return Transport.UKOpenWagon().country; }
+
+    public String transportYear() { return Transport.UKOpenWagon().year; }
+
+    public float transportTopSpeed() {
+        return Transport.UKOpenWagon().topSpeed;
+    }
+
+    public boolean isFictional() {
+        return Transport.UKOpenWagon().fictional;
+    }
+
+    @Override
+    public String[] additionalItemText() {
+        {return new String[]{RailUtility.translate(Transport.UKOpenWagon().additionalTextTitle) + Transport.UKOpenWagon().additionalText,
+                RailUtility.translate(Transport.UKOpenWagon().additionalTextTitle2) + Transport.UKOpenWagon().additionalText2};}
+    }
+    public int getInventoryRows() {
+        return Transport.UKOpenWagon().rows;
+    }
+
+    public TrainsInMotion.transportTypes getType() {
+        return TrainsInMotion.transportTypes.FREIGHT;
+    }
+
+    public float[][] getRiderOffsets() {
+        return (float[][])null;
+    }
+
+    public float[] getHitboxSize() {
+        return new float[]{2.95f, 1.4f, 1.4F};
+    }
+
+    public float getPistonOffset() {
+        return 0.0F;
+    }
+
+    public ModelBase[] getModel() {
+        return new ModelBase[]{new UKopenwagon2()};
+    }
+
+    public Item getItem() {
+        return thisItem;
+    }
+
+
+}
