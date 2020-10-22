aafd0227c7a451a5d14a2f247c1b2680d9214f0d
Henrik <henikojgaard@gmail.com>
2020-10-22 23:21:05 +0200

    Minor Fixes
    Signed-off-by: Henrik <henikojgaard@gmail.com>

--- src/main/java/wwcp/entities/freight/EntityHbbillns.java
+++ src/main/java/wwcp/entities/freight/EntityHbbillns.java
@@ -39,7 +39,7 @@ public class EntityHbbillns extends GenericRailTransport {
     }
 
     public float[] bogieLengthFromCenter() {
-        return new float[]{6.0F, -6.0F};
+        return new float[]{1.0F, -1.0F};
     }
 
     public float getRenderScale() {
