dba54af42aaab4ee81f747b98827057bdd494823
Henrik Øjgaard <64219297+MarkPurayahII@users.noreply.github.com>
2022-04-10 11:29:43 +0200

    Minor fixes of model and skins

--- src/main/java/wwcp/entities/locomotives/diesels/EntityV200.java
+++ src/main/java/wwcp/entities/locomotives/diesels/EntityV200.java
@@ -1,4 +1,4 @@
-package wwcp.entities.Advent;
+package wwcp.entities.locomotives.diesels;
     //This is a documentation class for copy pasting into a Diesel train class.
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
